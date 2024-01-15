e76226289917587f142fc26ca9e73722fd8722c7
oriolbaron <131920634+oriolbaron@users.noreply.github.com>
2024-01-15 22:08:54 +0100

    commit-236 update script

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ function loadYouTubeIframe(container) {
     var thumbnail = container.querySelector('.youtube-thumbnail');
     var iframe = container.querySelector('.youtube-iframe');
     if (!iframe.hasAttribute('src')) {
-      iframe.setAttribute('src', iframe.getAttribute('data-src'));
+      iframe.setAttribute('src', iframe.getAttribute('src'));
       iframe.style.display = 'block';
       thumbnail.style.display = 'none';
     }
